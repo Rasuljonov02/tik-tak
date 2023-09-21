a0116c3981d81e1f017c25c6160e00487754445a
JahongirOdilov <odilovjahongir2001@gmail.com>
2023-09-21 21:02:59 +0500

    js change

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const content = document.querySelector(".contentlar");
+const result = document.querySelector(".result");
 
 for (let i = 1; i <= 9; i++) {
 	content.innerHTML += `<div class="cell"></div>`;
 }
-let cunt = 0;
+let count = 0;
 
 content.onclick = function (e) {
 	if (e.target.className == "cell") {
-		if (cunt % 2 == 0) {
+		if (count % 2 == 0 && e.target.innerText ==="") {
 			e.target.innerText = "x";
-		} else {
+		} else if (count % 2 == 1 && e.target.innerText ==="") {
 			e.target.innerText = "o";
 		}
-		cunt++;
+		count++;
 		mer();
 	}
 };
@@ -20,29 +21,32 @@ content.onclick = function (e) {
 function mer() {
 	let mem = document.getElementsByClassName("cell");
 	if (
-		(mem[0].innerText == "x" && mem[1].innerText == "x" && mem[2].innerText == "x")(
-			mem[3].innerText == "x" && mem[4].innerText == "x" && mem[5].innerText == "x"
-		)(mem[6].innerText == "x" && mem[7].innerText == "x" && mem[8].innerText == "x")(
-			mem[0].innerText == "x" && mem[3].innerText == "x" && mem[6].innerText == "x"
-		)(mem[1].innerText == "x" && mem[4].innerText == "x" && mem[7].innerText == "x")(
-			mem[2].innerText == "x" && mem[5].innerText == "x" && mem[8].innerText == "x"
-		)(mem[0].innerText == "x" && mem[4].innerText == "x" && mem[8].innerText == "x")(
-			mem[2].innerText == "x" && mem[4].innerText == "x" && mem[6].innerText == "x"
-		)
+		(mem[0].innerText == "x" && mem[1].innerText == "x" && mem[2].innerText == "x") ||
+		(mem[3].innerText == "x" && mem[4].innerText == "x" && mem[5].innerText == "x") ||
+		(mem[6].innerText == "x" && mem[7].innerText == "x" && mem[8].innerText == "x") ||
+		(mem[0].innerText == "x" && mem[3].innerText == "x" && mem[6].innerText == "x") ||
+		(mem[1].innerText == "x" && mem[4].innerText == "x" && mem[7].innerText == "x") ||
+		(mem[2].innerText == "x" && mem[5].innerText == "x" && mem[8].innerText == "x") ||
+		(mem[0].innerText == "x" && mem[4].innerText == "x" && mem[8].innerText == "x") ||
+		(mem[2].innerText == "x" && mem[4].innerText == "x" && mem[6].innerText == "x")
 	) {
-		alert("Winner is X");
+		result.innerText = " Winner is X"
+		result.style.display = "block"
+		// document.body.style.backdropFilter = 10px;
 	} else if (
-		(mem[0].innerText == "o" && mem[1].innerText == "o" && mem[2].innerText == "o")(
-			mem[3].innerText == "o" && mem[4].innerText == "o" && mem[5].innerText == "o"
-		)(mem[6].innerText == "o" && mem[7].innerText == "o" && mem[8].innerText == "o")(
-			mem[0].innerText == "o" && mem[3].innerText == "o" && mem[6].innerText == "o"
-		)(mem[1].innerText == "o" && mem[4].innerText == "o" && mem[7].innerText == "o")(
-			mem[2].innerText == "o" && mem[5].innerText == "o" && mem[8].innerText == "o"
-		)(mem[0].innerText == "o" && mem[4].innerText == "o" && mem[8].innerText == "o")(
-			mem[2].innerText == "o" && mem[4].innerText == "o" && mem[6].innerText == "o"
-		)
+		(mem[0].innerText == "o" && mem[1].innerText == "o" && mem[2].innerText == "o") ||
+		(mem[3].innerText == "o" && mem[4].innerText == "o" && mem[5].innerText == "o") ||
+		(mem[6].innerText == "o" && mem[7].innerText == "o" && mem[8].innerText == "o") ||
+		(mem[0].innerText == "o" && mem[3].innerText == "o" && mem[6].innerText == "o") ||
+		(mem[1].innerText == "o" && mem[4].innerText == "o" && mem[7].innerText == "o") ||
+		(mem[2].innerText == "o" && mem[5].innerText == "o" && mem[8].innerText == "o") ||
+		(mem[0].innerText == "o" && mem[4].innerText == "o" && mem[8].innerText == "o") ||
+		(mem[2].innerText == "o" && mem[4].innerText == "o" && mem[6].innerText == "o")
 	) {
-		alert("Winner is O");
+		result.innerText = " Winner is O"
+		result.style.display = "block"
+		// document.body.style.backdropFilter = 10px;
+
 		return;
 	}
 }
